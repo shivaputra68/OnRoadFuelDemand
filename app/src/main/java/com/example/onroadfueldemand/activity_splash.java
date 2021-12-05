22375a43b23ec749467b7711b772ea5445dff90d
shivaputra68 <shivakamt888@gmail.com>
2021-12-05 09:59:56 +0530

    Back4App configuration is done

--- app/src/main/java/com/example/onroadfueldemand/activity_splash.java
+++ app/src/main/java/com/example/onroadfueldemand/activity_splash.java
@@ -2,13 +2,25 @@ package com.example.onroadfueldemand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class activity_splash extends AppCompatActivity {
 
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+            handler = new Handler();
+            handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(activity_splash.this,MainActivity.class);
+                startActivity(i);
+            }
+        },5000);
     }
 }
