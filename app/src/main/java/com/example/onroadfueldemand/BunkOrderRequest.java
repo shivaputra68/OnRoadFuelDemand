6c680ad68742142e08fbeb4bd71241d3c4114ed5
shivaputra68 <shivakamt888@gmail.com>
2022-01-31 12:25:32 +0530

    bunk home page history and update

--- app/src/main/java/com/example/onroadfueldemand/BunkOrderRequest.java
+++ app/src/main/java/com/example/onroadfueldemand/BunkOrderRequest.java
@@ -6,9 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
-
 import Interfaces.OrderFuelRecyclerClickListner;
 import operations.BunkOrder;
 import operations.BunkOrderAdapter;
