e853f1e50a6be3b020c9d38cf65834bd411485ec
shivaputra68 <shivakamt888@gmail.com>
2022-01-31 18:24:38 +0530

    bunk home page history and update

--- app/src/main/java/com/example/onroadfueldemand/ViewOrders.java
+++ app/src/main/java/com/example/onroadfueldemand/ViewOrders.java
@@ -7,14 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import operations.OrderViewAdapter;
 import operations.ViewOrder;
@@ -39,23 +35,20 @@ public class ViewOrders extends AppCompatActivity {
     }
 
     private void setViewOrders() {
-        ParseQuery<ParseObject> obj = new ParseQuery<ParseObject>("order");
-        obj.whereEqualTo("customer_name", ParseUser.getCurrentUser().getUsername());
-        obj.orderByDescending("updateAt");
-        obj.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                for(ParseObject object : objects){
-                    System.out.println("*******************"+object.get("bunk_name").toString()+"****************");
-                    orderHistory.add(new ViewOrder(object.get("bunk_name").toString(), object.get("fuel_type").toString(),
-                            object.getObjectId().toString(), object.get("total_amount").toString(),object.get("status").toString(),
-                            object.get("date").toString(),object.get("quantity").toString()));
-                }
+
+        ParseQuery<ParseObject> obj = ParseQuery.getQuery("order");
+        obj.findInBackground(((objects, e) -> {
+
+            for(int i=0;i< objects.size();i++){
+                System.out.println("*******************"+objects.get(i).get("bunk_name").toString()+"****************");
+                orderHistory.add(new ViewOrder(objects.get(i).get("bunk_name").toString(), objects.get(i).get("fuel_type").toString(),
+                        objects.get(i).getObjectId().toString(), objects.get(i).get("total_amount").toString(),objects.get(i).get("status").toString(),
+                        objects.get(i).get("date").toString(),objects.get(i).get("quantity").toString()));
             }
-        });
 
             OrderViewAdapter adapter = new OrderViewAdapter(this, orderHistory);
             recyclerView.setAdapter(adapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }));
     }
 }
